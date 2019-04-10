3ca0652943661fbe7a8b1fcf9b46fa52452a395e
Jet <jet23058@gmail.com>
2019-04-10 15:43:34 +0800

    補上自定義alias

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // 自訂回傳格式
+        App\Providers\CustomResponseServiceProvider::class,
     ],
 
     /*
@@ -201,6 +203,7 @@ return [
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
+        'CustomResopnse' => App\Facades\CustomResponse::class,
         'DB' => Illuminate\Support\Facades\DB::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
